3456cdeb033efd5967d72b8f80104cefb5f239dd
Calvin Williams <calvin.william1012@gmail.com>
2020-07-30 22:43:14 -0400

    making api stuff environment variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-exports.ACCESS_TOKEN = "GroupMe access token";
-exports.BOT_ID = "GroupMe bot ID";
-exports.GROUP_ID = "GroupMe group id";
+exports.ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+exports.BOT_ID = process.env.BOT_ID;
+exports.GROUP_ID = process.env.GROUP_ID;
 exports.GIHPY_KEY = 'Giphy API key';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
